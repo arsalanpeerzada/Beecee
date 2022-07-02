9e1a1fd4f6f14bd3bd4efddaaf5821734f6a0d2a
Arsalan Peerzada <peerzada_1994@live.com>
2022-07-02 18:55:50 +0500

    Initial commit

---
+++ app/src/main/java/com/example/beecee/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.beecee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+// ...
+// Initialize Firebase Auth
+        auth = Firebase.auth
+
+    }
+}
